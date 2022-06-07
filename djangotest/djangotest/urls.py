7c85ec3e6fe4788cbfc67c70683497952b03d309
Pranav Nair <pranavgn@gmail.com>
2022-06-07 13:16:52 +0530

    added global rest support

--- djangotest/djangotest/urls.py
+++ djangotest/djangotest/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('', include('test1.urls')),
     path('admin/', admin.site.urls),
-
+    path('api-auth/', include('rest_framework.urls')),
 ]
